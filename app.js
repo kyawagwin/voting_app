bfc02dccaadad21f81795d1da51078cac5d6eeeb
Kyaw Aung Win <kyawagwin@gmail.com>
2017-01-09 22:42:57 +0800

    second component: Product

--- app.js
+++ app.js
@@ -2,11 +2,34 @@ const ProductList = React.createClass({
 	render: function() {
 		return (
 			<div className='ui items'>
-				Hello, I am a basic React component.
+				<Product />
 			</div>
 		);
 	},
 });
+
+const Product = React.createClass({
+	render: function() {
+		
+		return (
+			<div className='item'>
+				<div className='image'>
+					<img src='images/products/image-aqua.png' />
+				</div>
+				<div className='middle aligned content'>
+					<div className='description'>
+						<a>Fort Night</a>
+						<p>Authentic actors, delivered in just two weeks.</p>
+					</div>
+					<div className='extra'>
+						<span>Submitted by:</span>
+						<img className='ui avatar image' src='images/avatars/daniel.jpg' />
+					</div>
+				</div>
+			</div>
+		);
+	}
+})
 
 ReactDOM.render(
 	<ProductList />, 
