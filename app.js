e9dcba86191ae9ace9fdb490d1a79000227b336e
Kyaw Aung Win <kyawagwin@gmail.com>
2017-01-10 23:05:27 +0800

    app interaction

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
+
 const ProductList = React.createClass({
+	getInitialState: function() {
+		return {
+			products: []
+		};
+	},
+
+	componentDidMount: function() {
+		this.updateState();
+	},
+
+	updateState: function() {
+		const products = Data.sort((a, b) => {
+			return b.votes - a.votes;
+		});
+		this.setState({ products: products });
+	},
+
+	handleProductUpVote: function(productId) {
+		Data.forEach((el) => {
+			if(el.id === productId) {
+				el.votes = el.votes + 1;
+				return;
+			}
+		});
+		this.updateState();
+	},
+
+	handleProductDownVote: function(productId) {
+		Data.forEach((el) => {
+			if(el.id === productId) {
+				el.votes = el.votes - 1;
+				return;
+			}
+		});
+		this.updateState();
+	},
+
 	render: function() {
-		const products = Data.map((product) => {
+		const products = this.state.products.map((product) => {
 			return (
 				<Product 
 					key={'product-' + product.id}
@@ -11,6 +49,8 @@ const ProductList = React.createClass({
 					votes={product.votes}
 					submitter_avatar_url={product.submitter_avatar_url}
 					product_image_url={product.product_image_url}
+					onVote={this.handleProductUpVote}
+					onDownVote={this.handleProductDownVote}
 				/>
 			);
 		});
@@ -24,14 +64,31 @@ const ProductList = React.createClass({
 });
 
 const Product = React.createClass({
+	handleUpVote: function() {
+		this.props.onVote(this.props.id);
+	},
+
+	handleDownVote: function() {
+		this.props.onDownVote(this.props.id);
+	},
+
 	render: function() {
 		
 		return (
 			<div className='item'>
 				<div className='image'>
 					<img src={this.props.product_image_url} />
-				</div>
+				</div>			
 				<div className='middle aligned content'>
+					<div className='header'>
+						<a onClick={this.handleUpVote}>
+							<i className='large caret up icon'></i>
+						</a>					
+						{this.props.votes}
+						<a onClick={this.handleDownVote}>
+							<i className='large caret down icon'></i>
+						</a>
+					</div>
 					<div className='description'>
 						<a>{this.props.title}</a>
 						<p>{this.props.description}</p>
